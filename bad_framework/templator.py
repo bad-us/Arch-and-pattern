e0478950b0099141d82e7b2547a48584dbbc0b84
Bad <bad-us@rambler.ru>
2022-01-31 09:02:23 +0300

    Final

--- bad_framework/templator.py
+++ bad_framework/templator.py
@@ -1,9 +1,18 @@
 from jinja2 import Template
 import os
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
+
 
 
 def render(template_name, folder="templates", **kwargs):
-    file_path = os.path.join(folder, template_name)
-    with open(file_path, encoding="utf-8") as f:
-        template = Template(f.read())
+    # file_path = os.path.join(folder, template_name)
+    # with open(file_path, encoding="utf-8") as f:
+    #     template = Template(f.read())
+    # return template.render(**kwargs) Lesson 2
+    env = Environment()
+    # указываем папку для поиска шаблонов
+    env.loader = FileSystemLoader(folder)
+    # находим шаблон в окружении
+    template = env.get_template(template_name)
     return template.render(**kwargs)
